bd324a91d60dd3b5bf5517921830a4ab72fc0800
diofarrel <diofarrel.work@gmail.com>
2023-08-23 11:18:16 +0700

    Change view header for admin and user

--- resources/views/home/header.blade.php
+++ resources/views/home/header.blade.php
@@ -1,7 +1,11 @@
 <header class="header_section">
     <div class="container">
         <nav class="navbar navbar-expand-lg custom_nav-container ">
-            <a class="navbar-brand" href="{{url('/')}}"><img width="250" src="images/logo.png" alt="#" /></a>
+            <a class="nav-item" href="{{url('/')}}">
+                <form method="" action="">
+                    <input class="" type="submit" value="CV EKA JAYA">
+                </form>
+            </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class=""> </span>
             </button>
@@ -10,6 +14,12 @@
                     <li class="nav-item">
                         <a class="nav-link" href="{{url('/')}}">Home <span class="sr-only">(current)</span></a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('productpage') }}">Products</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('show_cart') }}">Cart</a>
+                    </li>
                     <!-- <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="true"> <span class="nav-label">Pages <span class="caret"></span></a>
                         <ul class="dropdown-menu">
@@ -17,18 +27,12 @@
                             <li><a href="testimonial.html">Testimonial</a></li>
                         </ul>
                     </li> -->
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ url('productpage') }}">Products</a>
-                    </li>
                     <!-- <li class="nav-item">
                         <a class="nav-link" href="blog_list.html">Blog</a>
                     </li> -->
-                    <li class="nav-item">
+                    <!-- <li class="nav-item">
                         <a class="nav-link" href="contact.html">Contact</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ url('show_cart') }}">Cart</a>
-                    </li>
+                    </li> -->
                     <!-- <form class="form-inline">
                         <button class="btn  my-2 my-sm-0 nav_search-btn" type="submit">
                             <i class="fa fa-search" aria-hidden="true"></i>
