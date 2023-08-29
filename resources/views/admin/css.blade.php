dac7cbc359d7c6976a93cc82abcd6b21bb55dc4d
diofarrel <diofarrel.work@gmail.com>
2023-08-29 10:14:00 +0700

    Update view in user and admin page

--- resources/views/admin/css.blade.php
+++ resources/views/admin/css.blade.php
@@ -1,6 +1,6 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<title>Corona Admin</title>
+<title>Admin - EKA JAYA</title>
 {{-- <!-- plugins:css -->
 <link rel="stylesheet" href="admin/assets/vendors/mdi/css/materialdesignicons.min.css">
 <link rel="stylesheet" href="admin/assets/vendors/css/vendor.bundle.base.css">
@@ -33,4 +33,4 @@
 <!-- Layout styles -->
 <link rel="stylesheet" href="{{ asset('admin/assets/css/style.css') }}">
 <!-- End layout styles -->
-<link rel="shortcut icon" href="{{ asset('admin/assets/images/favicon.png') }}" />
+<link rel="shortcut icon" href="images/logo.png" />
